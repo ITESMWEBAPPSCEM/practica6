81248e564c43c908503617bc05932d3fa2b1404c
Arturo Amador Paulino <amadorassassin@gmail.com>
2019-02-20 17:06:15 -0600

    estructura ejs

--- base.js
+++ base.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 const path = require('path');
 const session = require('express-session');
@@ -41,4 +41,27 @@ app.get('/mi_lista', (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Escuchando en puerto ${port}...`));
+app.listen(port, () => console.log(`Escuchando en puerto ${port}...`));*/
+
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function (req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
